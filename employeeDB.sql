b123015d5741ff4d4a06ece7a5596008b6195af4
Sammy Kroner <sammyk118@gmail.com>
2021-01-28 20:36:09 -0800

    sql with proper foreign keys

--- employeeDB.sql
+++ employeeDB.sql
@@ -6,21 +6,24 @@ CREATE TABLE departments(
     id INTEGER(10) AUTO_INCREMENT NOT NULL,
     name VARCHAR(50),
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE roles(
     id INTEGER(10) AUTO_INCREMENT NOT NULL,
     title VARCHAR(50),
     salary DECIMAL(10, 4),
-    department_id,
+    department_id INTEGER(10),
+    FOREIGN KEY (department_id) REFERENCES departments(id),
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employees(
     id INTEGER(10) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR (50),
     last_name VARCHAR(50),
-    role_id,
-    manager_id,
+    role_id INTEGER(10),
+    manager_id INTEGER(10),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     PRIMARY KEY (id)
-)
+);
