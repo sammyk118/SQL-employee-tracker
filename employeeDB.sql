5d8e0c045048774af9b22c8436ef8d2ba10600e9
Sammy Kroner <sammyk118@gmail.com>
2021-01-29 18:01:09 -0800

    updated sql and js start

--- employeeDB.sql
+++ employeeDB.sql
@@ -3,26 +3,26 @@ CREATE DATABASE workplace_db;
 USE workplace_db;
 
 CREATE TABLE departments(
-    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(50),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles(
-    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(50),
     salary DECIMAL(10, 4),
-    department_id INTEGER(10),
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES departments(id),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE employees(
-    id INTEGER(10) AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR (50),
-    last_name VARCHAR(50),
-    role_id INTEGER(10),
-    manager_id INTEGER(10),
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR (50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employees(id),
     FOREIGN KEY (role_id) REFERENCES roles(id),
     PRIMARY KEY (id)
